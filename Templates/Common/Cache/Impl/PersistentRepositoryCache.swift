17a82124c764b207c950a3c79214b3d273ed2e60
Igor Shalovsky <igorshavlovsky@gmail.com>
2019-12-09 13:14:35 +0300

    temp commit

---
+++ Templates/Common/Cache/Impl/PersistentRepositoryCache.swift
@@ -0,0 +1,13 @@
+//
+//  PersistentRepositoryCache.swift
+//  Templates
+//
+//  Created by Igor Shavlovsky on 11/29/19.
+//  Copyright © 2019 Igor Shavlovsky. All rights reserved.
+//
+
+import UIKit
+
+class PersistentRepositoryCache: NSObject {
+
+}
